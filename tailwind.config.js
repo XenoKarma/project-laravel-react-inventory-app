51d88263aad17ecd127afd6bddda8c20cddb8898
XenoKarma <andikapurnama1803@gmail.com>
2026-04-17 21:51:38 +0700

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class'],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -12,6 +13,13 @@ export default {
 
     theme: {
         extend: {
+            colors:{
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
